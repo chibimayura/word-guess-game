7995fe8ab026eb7f23c3fb411980d80932d2bd27
chibimayura <chibimayura@gmail.com>
2018-06-14 18:09:57 -0700

    add guess log to log user guess & changed background image placement

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -6,7 +6,7 @@
 
 	//stores the possible answers
 	var answers = ["bubbles", "blossom", "buttercup", "ProfessorUtonium", "Major", "MissBellum", "MojoJojo", "Him", "Octi"];
-
+	var alphabet = "abcdefghijklmnopqrstuvwxyz";
 	var currentAns; //stores current answer
 	var userGuess = []; //stores user's guess
 	var guessAns; //stores the _ according to the length of the answer and fills the array when found right answer
@@ -19,7 +19,6 @@
 	var game_div = document.querySelector("#game");
 
 	score_div.innerText = score;
-	life_div.innerText = lifePoints;
 	start.innerText = "Press any key to start game";
 
 	//generates a random answer from the answer array
@@ -32,7 +31,10 @@
 
 	//generates the _ to show the user what word they need to guess
 	function answerGenerator(){
-		currentAns = randomAns();
+
+		if(!gameStart){
+			currentAns = randomAns();
+		}
 
 		for(var i = 0; i < currentAns.length; i++){
 			guessAns[i] = "_";
@@ -44,21 +46,25 @@
 	//logs and checks to see if the user has already made the guess and subtracts life points accordingly
 	function logAndCompareGuess(event){
 		var key = event.key;
-		userGuess.push(event.key);
 
 		//detects if user pressed any key to start the game
 		if(!gameStart){
 			answerGenerator();
 			start.innerText = " GAME STARTED";
+			life_div.innerText = lifePoints;
 			gameStart = true;
 		}
 
-
+		if(gameStart) {
+			if(userGuess.indexOf(key) == -1 && alphabet.indexOf(key) > -1){
+				userGuess.push(key);
+				guesses.innerText = userGuess;
+			}
+		}
 	}
 
 	document.onkeyup = logAndCompareGuess;
 
-	//stores a set of answer for user to guess
 	//stores user's guess and if the user has already guess(indexOf) the letter do not subtract guess count left
 	//if user guesses wrong subtract guess count left
 	//if user guesses correctly add win score && play victory song
